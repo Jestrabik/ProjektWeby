5c1fe39ecf0119b01426706ffff5c8229893e480
FojtikPetroauh <fojtik_petr@oauh.cz>
2025-04-25 12:06:13 +0200

    Database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'fojtik',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'fojtik',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
